89d35d5f18029a4af39941171472e3c06f658af7
Jamppa88 <repse88@gmail.com>
2017-01-11 13:29:56 +0200

    Exercises T4 commented and T5 completed

--- T4/T4.cs
+++ T4/T4.cs
@@ -1,28 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 /* Tee ohjelma, jossa kysytään käyttäjältä tämän ikä.
  * Jos ikä on alle 18 vuotta, tulosta "alaikäinen", jos se on 18-65 vuotta,
  * tulosta "aikuinen", muussa tapauksessa tulosta "seniori".
+ * 
+ * Jani Kerttula K8958
+ * 11.1.2017
  */
 
 namespace T4
 {
+    // Huvin vuoksi erillenen class iälle
     class Henkilo
     {
         public int IKA { get; set; }
     }
-        class T4
+    class T4
     {
         static void Main(string[] args)
         {
             Henkilo henk = new Henkilo();
             Console.Write("Anna ikäsi > ");
             henk.IKA = int.Parse(Console.ReadLine());
-
+            // yksinkertainen vertailu iästä
             if (henk.IKA < 18)
             {
                 Console.WriteLine("Olet alaikäinen!");
---
+++ T5/T5.cs
@@ -0,0 +1,39 @@
+﻿/* Tee ohjelma, joka näyttää annetun sekuntimäärän tunteina, minuutteina ja sekunteina. Aikamääre sekuntteina kysytään käyttäjältä.
+ *
+ * Jani Kerttula K8958
+ * 11.1.2017
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T5
+{
+    class T5
+    {
+        // Annetaan yhteinen muuttuja metodeille
+        static int t;
+        static void Main(string[] args)
+        {
+            Console.Write("Anna sekunttimäärä > ");
+            t = int.Parse(Console.ReadLine());
+            CalcTime();
+            Console.ReadLine();
+        }
+        // Aliohjelma, joka laskee ajan haluttuun muotoon
+        static void CalcTime()
+        {
+            int h, m, s;
+            // Integerejä jaettaessa laskutoimitus jättää desimaalit pois, = profit!
+            h = t / 360;
+            m = (t - h * 360) / 60;
+            s = t - h * 360 - m * 60;
+
+            Console.WriteLine("Antamasi sekunttiaika voidaan ilmaista muodossa: {0} tuntia {1} minuuttia {2} sekunttia", h, m, s);
+
+        }
+    }
+}
