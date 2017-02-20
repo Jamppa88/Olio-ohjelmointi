35fb4fa58e936e22e4d604af30a6b8d4b41b8891
Jamppa88 <repse88@gmail.com>
2017-02-20 13:11:49 +0200

    Add Labra12

---
+++ Labra12/Playground/ViewModel/StudentViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Playground.Model;
+using System.Collections.ObjectModel;
+
+namespace Playground.ViewModel
+{
+    public class StudentViewModel
+    {
+        public ObservableCollection<Student> Students
+        {
+            get;
+            set;
+        }
+        public void LoadStudents()
+        {
+            ObservableCollection<Student> students = new ObservableCollection<Student>();
+            students.Add(new Student { FirstName = "Mark", LastName = "Allen", AsioId = "K0011" });
+            students.Add(new Student { FirstName = "John", LastName = "Doe", AsioId = "B4567" });
+            students.Add(new Student { FirstName = "Linda", LastName = "Kernell", AsioId = "C3421" });
+            Students = students;
+        }
+    }
+}
